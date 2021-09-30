8411d25087a7185ebeca9068c0a463a87200e357
repair-cat <simf61@yandex.ru>
2021-09-30 12:16:41 +0500

    обновлен вывод моделей в панели админа Джанго

--- health_gate/food/admin.py
+++ health_gate/food/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}   # для автозаполнения поля slug у модели Category
+
+
+@admin.register(Kitchen)
+class KitchenAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}   # для автозаполнения поля slug у модели Kitchen
+
+
+class IngredientAdmin(admin.TabularInline):
+    model = Ingredient
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}  # для автозаполнения поля slug у модели Recipe
+    filter_horizontal = ('tags',)               # выбор тэгов
+    inlines = [IngredientAdmin, ]
+
+
+admin.site.register(Tag)
+admin.site.register(Subtype)
+admin.site.register(Filter)
+admin.site.register(CookStep)
--- health_gate/user/admin.py
+++ health_gate/user/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
+from .models import *
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(DjangoUserAdmin):
+    """Кастомная модель Админа с полем номера телефона вместо логина"""
+
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        (_('Personal info'), {'fields': ('full_name',
+                                         'nickname',
+                                         'email',
+                                         'address',
+                                         'about',
+                                         'photo',
+                                         'tags',
+                                         'twitter',
+                                         'facebook',
+                                         'instagram',
+                                         'youtube',
+                                         'patreon',
+                                         )}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('phone_number', 'full_name', 'is_staff', 'is_active')
+    search_fields = ('phone_number',)
+    ordering = ('phone_number',)
