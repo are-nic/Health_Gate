079549a7d6608b2f0758bc43ea9cbe5cec3b46a0
repair-cat <simf61@yandex.ru>
2021-10-02 09:57:12 +0500

    зарегистрирована модель Заказа

--- health_gate/order/admin.py
+++ health_gate/order/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class OrderRecipeInline(admin.TabularInline):
+    model = OrderRecipe
+    raw_id_fields = ['recipe']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'created_at', 'updated_at', 'paid']
+    inlines = [OrderRecipeInline, ]
