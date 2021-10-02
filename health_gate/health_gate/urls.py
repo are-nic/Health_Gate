05e3c669b759f8388a2631316c9f29099cc7568e
repair-cat <simf61@yandex.ru>
2021-10-02 10:00:20 +0500

    добавлены роуты для АПИ

--- health_gate/health_gate/urls.py
+++ health_gate/health_gate/urls.py
@@ -1,21 +1,14 @@
-"""health_gate URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include('api.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:      # если проект в режиме Дебаг, то директории для медиафайлов здесь
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
