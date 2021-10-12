65a0da07ec999ca054707342b4c2386f22c58c43
repair-cat <simf61@yandex.ru>
2021-10-12 08:08:13 +0500

    добавлены сериализаторы заказа и плана питания

--- health_gate/order/serializers.py
+++ health_gate/order/serializers.py
@@ -1,27 +1,49 @@
 from rest_framework import serializers
-from .models import Order, OrderRecipe, OrderProduct
-
-
-class OrderRecipeSerializer(serializers.ModelSerializer):
-    """
-    Рецепт заказа
-    """
-    class Meta:
-        model = OrderRecipe
-        fields = '__all__'
+from food.models import Recipe
+from .models import Order, OrderRecipe, OrderProduct, MealPlanRecipe
+from food.serializers import ChoiceField
 
 
 class OrderProductSerializer(serializers.ModelSerializer):
     """
     Продукт заказа
     """
+    recipe = serializers.SlugRelatedField(slug_field='title', queryset=Recipe.objects.all())
+
     class Meta:
         model = OrderProduct
         fields = '__all__'
 
 
+class OrderRecipeSerializer(serializers.ModelSerializer):
+    """
+    Рецепт заказа
+    """
+    # products = OrderProductSerializer(many=True)
+    recipe = serializers.SlugRelatedField(slug_field='title', queryset=Recipe.objects.all())
+
+    class Meta:
+        model = OrderRecipe
+        exclude = ['order']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    """Заказ"""
+
+    customer = serializers.CharField(source="customer.phone_number", read_only=True)
+    recipes = OrderRecipeSerializer(many=True)
+    pay_method = ChoiceField(choices=Order.PAY_METHOD)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
 class OrderListSerializer(serializers.ModelSerializer):
     """Список Заказов"""
+    customer = serializers.CharField(source="customer.phone_number", read_only=True)
+    pay_method = ChoiceField(choices=Order.PAY_METHOD)
+
     class Meta:
         model = Order
         fields = '__all__'
@@ -32,8 +54,24 @@ class OrderDetailSerializer(serializers.ModelSerializer):
 
     customer = serializers.CharField(source="customer.phone_number", read_only=True)
     recipes = OrderRecipeSerializer(many=True)
-    products = OrderProductSerializer(many=True)
+    pay_method = ChoiceField(choices=Order.PAY_METHOD)
 
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class MealPlanRecipeSerializer(serializers.ModelSerializer):
+    """Рецепт плана питания"""
+
+    owner = serializers.ReadOnlyField(source='owner.phone_number')
+    recipe = serializers.SlugRelatedField(slug_field='name', queryset=Recipe.objects.all())
+
+    class Meta:
+        model = MealPlanRecipe
+        fields = '__all__'
+
+    def validate_recipe(self, value):
+        if value.user != self.context['request'].user:
+            raise serializers.ValidationError('Неверный id рецепта')
+        return value
