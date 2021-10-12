48c09c8f4470b5079c38380ddb30ac4c8d871433
repair-cat <simf61@yandex.ru>
2021-10-12 08:06:34 +0500

    добавлена модель плана питания

--- health_gate/order/models.py
+++ health_gate/order/models.py
@@ -14,7 +14,7 @@ class Order(models.Model):
         ('UPON_RECEIPT', 'Оплатить наличными или картой при получении'),
         ('ONLINE', 'Оплатить онлайн'),
     ]
-    customer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, verbose_name='Покупатель')
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Покупатель', null=True)
     name = models.CharField(max_length=50, verbose_name='Имя', blank=True, null=True)
     address = models.CharField(max_length=250, verbose_name='Адрес доставки', null=True)
     phone_number = models.PositiveIntegerField(verbose_name='Телефон', blank=True, null=True)
@@ -32,7 +32,7 @@ class Order(models.Model):
         db_table = 'order'
 
     def __str__(self):
-        return 'Заказ от: {}'.format(self.customer.phone_number)
+        return self.customer.phone_number
 
 
 class OrderRecipe(models.Model):
@@ -40,7 +40,7 @@ class OrderRecipe(models.Model):
     Модель Рецепта в заказе
     """
     order = models.ForeignKey(Order, related_name='recipes', on_delete=models.CASCADE, verbose_name='Заказ')
-    recipe = models.ForeignKey(Recipe, related_name='recipe_items', on_delete=models.CASCADE, verbose_name='Рецепт')
+    recipe = models.ForeignKey(Recipe, related_name='order_recipe', on_delete=models.CASCADE, verbose_name='Рецепт')
     qty = models.PositiveIntegerField(verbose_name='Кол-во порций')
 
     class Meta:
@@ -50,16 +50,17 @@ class OrderRecipe(models.Model):
         db_table = 'order_recipe'
 
     def __str__(self):
-        return '{}'.format(self.recipe)
+        return self.recipe.title
 
 
 class OrderProduct(models.Model):
     """
     Модель Продукта в заказе
     """
+    recipe = models.ForeignKey(OrderRecipe, related_name='order_recipe', on_delete=models.CASCADE,
+                               verbose_name='Рецепт')
     order = models.ForeignKey(Order, related_name='products', on_delete=models.CASCADE, verbose_name='Заказ')
-    recipe = models.ForeignKey(Recipe, related_name='recipe_item', on_delete=models.CASCADE, verbose_name='Рецепт')
-    product = models.ForeignKey(Product, related_name='product_item', on_delete=models.CASCADE, verbose_name='Продукт')
+    product = models.ForeignKey(Product, related_name='order_product', on_delete=models.CASCADE, verbose_name='Продукт')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     qty = models.PositiveIntegerField(verbose_name='Кол-во единиц')
     delivery_datetime = models.DateTimeField(verbose_name='Дата и время доставки', default=None)
@@ -71,4 +72,23 @@ class OrderProduct(models.Model):
         db_table = 'order_product'
 
     def __str__(self):
-        return '{}'.format(self.product)
+        return self.product.name
+
+
+class MealPlanRecipe(models.Model):
+    """
+    Рецепт плана питания
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец плана', related_name='owner')
+    recipe = models.ForeignKey(Recipe, related_name='recipe_plan', on_delete=models.CASCADE, verbose_name='Рецепт')
+    qty = models.PositiveIntegerField(verbose_name='Кол-во порций')
+    date = models.DateField(verbose_name='Плановая дата')
+
+    class Meta:
+        ordering = ('owner',)
+        verbose_name = 'Рецепт плана питания'
+        verbose_name_plural = 'Рецепты плана питания'
+        db_table = 'meal_plan_recipes'
+
+    def __str__(self):
+        return self.recipe.title
