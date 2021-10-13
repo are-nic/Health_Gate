6e223eb5b87fc9a61d8b1cbe3d84e38200dee804
root <root@151-248-112-170.xen.vps.regruhosting.ru>
2021-10-13 12:18:40 +0000

    staticfiles

--- health_gate/health_gate/settings.py
+++ health_gate/health_gate/settings.py
@@ -123,13 +123,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+#
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#
+# LOGIN_REDIRECT_URL = 'home'
+# STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
-MEDIA_URL = 'media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-LOGIN_REDIRECT_URL = 'home'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+
+if DEBUG == True:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
