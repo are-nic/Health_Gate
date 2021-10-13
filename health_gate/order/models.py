1cefbb5d53209988149891eddfa89d112d4cc99f
repair-cat <simf61@yandex.ru>
2021-10-13 23:20:17 +0500

    изменены настройки некоторых полей моделей

--- health_gate/order/models.py
+++ health_gate/order/models.py
@@ -58,7 +58,7 @@ class OrderProduct(models.Model):
     Модель Продукта в заказе
     """
     order = models.ForeignKey(Order, related_name='products', on_delete=models.CASCADE, verbose_name='Заказ')
-    recipe = models.ForeignKey(OrderRecipe, related_name='products', on_delete=models.CASCADE, verbose_name='Рецепт')
+    recipe = models.ForeignKey(OrderRecipe, related_name='products', on_delete=models.CASCADE, verbose_name='Рецепт заказа')
     product = models.ForeignKey(Product, related_name='order_product', on_delete=models.CASCADE, verbose_name='Продукт')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     qty = models.PositiveIntegerField(verbose_name='Кол-во единиц')
@@ -78,7 +78,7 @@ class MealPlanRecipe(models.Model):
     """
     Рецепт плана питания
     """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец плана', related_name='owner')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец плана', related_name='plan')
     recipe = models.ForeignKey(Recipe, related_name='recipe_plan', on_delete=models.CASCADE, verbose_name='Рецепт')
     qty = models.PositiveIntegerField(verbose_name='Кол-во порций')
     date = models.DateField(verbose_name='Плановая дата')
