855b4be3ddb2f4483febb7965ebacc18c5682eb5
Sirius_Mklain <sergeimakarenko5991@gmail.com>
2021-10-13 01:58:36 -0700

    settings environ

--- health_gate/health_gate/settings.py
+++ health_gate/health_gate/settings.py
@@ -8,12 +8,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&=t33p)@rx4(sx5!3ad84g%(#40@vktc=h5gb9mw)a77zx9%@'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dfhzdfgzdfgnmxc%&*%&^*&#$@@^*dkjfg'lglz'dfhdfgh")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(" ")
 
 
 # Application definition
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'health_gate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", "db.sqlite3"),
+        'USER': os.environ.get("SQL_USER", "heal_user"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", "123456"),
+        'HOST': os.environ.get("SQL_HOST", "localhost"),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
     }
 }
 
