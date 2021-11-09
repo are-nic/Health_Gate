d5ad454d24a88ae6d83a2ad6ffab191331ee2081
repair-cat <simf61@yandex.ru>
2021-11-09 09:08:33 +0500

    добавлены контрольные точки для Тэгов и Продуктов

--- health_gate/food/views.py
+++ health_gate/food/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from .models import Recipe, Comment, Ingredient, Product, CookStep
+from .models import Recipe, Comment, Ingredient, Product, CookStep, Tag
 from rest_framework import viewsets, generics, status
 from api.permissions import AuthorComment, RecipeOwner, IsOwnerRecipeIngredients, IsSuperUser
 
@@ -10,7 +10,8 @@ from .serializers import (RecipeListSerializer,
                           IngredientSerializer,
                           CommentSerializer,
                           ProductSerializer,
-                          CookStepSerializer)
+                          CookStepSerializer,
+                          TagSerializer)
 
 User = get_user_model()
 
@@ -55,13 +56,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         """переопределение метода для предоставления права создания рецепта только пользователям из группы "bloger" """
-        if request.user.groups.filter(name='bloger').exists():
+        if request.user.groups.filter(name='bloger').exists() or request.user.is_superuser:
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        return Response({"error": "Рецепты могут создавать пользователи из группы 'bloger'"})
+        return Response({"error": "Рецепты могут создавать пользователи из группы 'bloger' или Суперпользователь"})
 
     def perform_create(self, serializer):
         """
@@ -69,9 +70,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         привязка ингредиентов и шагов приготовления, созданных внутри рецепта.
         """
         serializer.save(owner=self.request.user)                            # сохраняем текущий рецепт
+
         ingredients_data = self.request.data.pop('ingredients')             # забираем из передаваемых данных список ингредиентов
         recipe = list(Recipe.objects.all())[-1]                             # присваиваем переменной только что созданный рецепт
         for ingredient_data in ingredients_data:                            # перебор всех переданных ингредиентов
+            ingredient_data['product'] = Product.objects.get(name=ingredient_data['product'])
             Ingredient.objects.create(recipe=recipe, **ingredient_data)     # создание ингредиента и привязка его к рецепту
 
         steps_data = self.request.data.pop('steps')                         # забираем из передаваемых данных список шагов приготовления                            # присваиваем переменной только что созданный рецепт
@@ -147,7 +150,45 @@ class CommentView(viewsets.ModelViewSet):
 
 
 class ProductView(viewsets.ModelViewSet):
-    """Просмотр, создание и редактирование Проодуктов"""
-    queryset = Product.objects.all()
+    """
+    Просмотр, создание и редактирование Проодуктов.
+    Сортировка по Магазину
+    Доступы: редактировать может только Суперпользователь,
+             просмотр доступен авторизованному пальзователю.
+    """
+    queryset = Product.objects.order_by('shop')
     serializer_class = ProductSerializer
-    permission_classes = [IsSuperUser]
+
+    def get_permissions(self):
+        """
+        Просмотр продуктов доступен любому авторизованному пользователю
+        Какие-либо дейсвтия над продуктами доступны только суперпользователю
+        :return: список разрешений
+        """
+        if self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsSuperUser]
+        return [permission() for permission in permission_classes]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    Просмотр, создание и редактирование Тэгов.
+    Доступы: редактировать может только Суперпользователь,
+             просмотр доступен авторизованному пальзователю.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_permissions(self):
+        """
+        Просмотр тэгов доступен всем (в т.ч. неавторизованным Пользователям)
+        Какие-либо дейсвтия над тэгами доступны только суперпользователю
+        :return: список разрешений
+        """
+        if self.request.method == 'GET':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsSuperUser]
+        return [permission() for permission in permission_classes]
