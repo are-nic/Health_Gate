36db437ef632518a9406fe708932fa9f2a097190
repair-cat <simf61@yandex.ru>
2021-12-30 11:18:51 +0500

    добавлены методы изменения данных пользователей

--- health_gate/user/views.py
+++ health_gate/user/views.py
@@ -1,5 +1,9 @@
+import uuid
+import base64
+
+from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, decorators, response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer
 from api.permissions import IsAccountOwner
@@ -10,7 +14,7 @@ User = get_user_model()
 
 class CurrentUserView(viewsets.ModelViewSet):
     """
-    Вывод текущего Пользовтеля
+    Вывод текущего Пользователя
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -26,6 +30,32 @@ class CurrentUserView(viewsets.ModelViewSet):
         Переопределение метода и направление его в retrieve для выдачи одного текущего юзера
         """
         return self.retrieve(request, *args, **kwargs)
+
+    @decorators.action(detail=False, methods=['put'])
+    def update_data(self, request):
+        photo = None
+        if request.data.get('photo'):
+            photo = request.data.pop('photo')
+
+        user_query = User.objects.filter(pk=request.user.id)
+        user_query.update(**request.data)
+
+        if photo:
+            file_format, image_str = photo.split(';base64,')
+            ext = file_format.split('/')[-1]
+            photo_file = ContentFile(base64.b64decode(image_str), name=f'{uuid.uuid4()}.{ext})')
+            user_object = user_query.first()
+            user_object.photo = photo_file
+            user_object.save()
+
+        return response.Response({"message": "success"})
+
+    @decorators.action(detail=False, methods=['put'])
+    def update_password(self, request):
+        user = User.objects.get(pk=request.user.id)
+        user.set_password(request.data.get('password'))
+        user.save()
+        return response.Response({"message": "success"})
 
 
 class UserView(viewsets.ModelViewSet):
